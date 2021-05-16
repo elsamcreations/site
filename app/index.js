c56ff610e5629496297821a5ba7174bce0869c7b
Clement Denis <le.mikmac@gmail.com>
2021-05-17 01:19:36 +0200

    app: sssssssss

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 import { Sheet } from './component/sheet.js'
 
-fetch('https://dev.oct.ovh:2096/x/couettes')
+fetch('https://dev.oct.ovh:2096/x/couette')
   .then((res) => res.json())
   .then((couettes) => document
     .querySelector('#sheets > article')
