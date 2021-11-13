7095037d27bb85419f190a63b6e28773752be78c
Clement Denis <le.mikmac@gmail.com>
2021-11-13 19:50:08 +0000

    update main page and add giveasheet redirection

--- script/build.js
+++ script/build.js
@@ -10,10 +10,10 @@ const result = buildSync({
   splitting: true,
 })
 
-const index = readFileSync('template/index.html', 'utf8')
-const replaced = index.replace(
+const giveasheet = readFileSync('template/giveasheet.html', 'utf8')
+const replaced = giveasheet.replace(
   /<!-- (.*)\[(.*)\](.*) -->\n(.*\n)/g,
   (_, open, path, close) => `${open}${readFileSync(path, 'utf8')}${close}`,
 )
 
-writeFileSync('public/index.html', replaced, 'utf8')
+writeFileSync('public/giveasheet.html', replaced, 'utf8')
